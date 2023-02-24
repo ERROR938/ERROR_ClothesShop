fe531c07b9bd2560e0ebcc3b9e7fae3437663b5e
Ψ. ERROR <88382503+ERROR938@users.noreply.github.com>
2023-02-24 17:03:59 +0100

    Update .sql

--- .sql
+++ .sql
@@ -1,7 +1,7 @@
+DROP TABLE IF EXISTS `saved_clothes`;
 CREATE TABLE IF NOT EXISTS `saved_clothes` (
   `name` varchar(100) NOT NULL,
   `skin` longtext NOT NULL,
   `owner` varchar(150) NOT NULL,
   PRIMARY KEY (`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-COMMIT;
+);
